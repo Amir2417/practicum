f2148670e74e4d6056f314d866dd403db6bc456e
Amir2417 <amirsohelto@gmail.com>
2022-01-19 23:29:53 +0600

    first commit

---
+++ src/Components/Firebase/firebase.config.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAteY4mQ_W6Y5PYLkf4yK3K2qiBZ9biwrs",
+  authDomain: "letstravel-ba00a.firebaseapp.com",
+  projectId: "letstravel-ba00a",
+  storageBucket: "letstravel-ba00a.appspot.com",
+  messagingSenderId: "142211876878",
+  appId: "1:142211876878:web:05228a2b4d0d0d271c545f",
+  // apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  // appId: process.env.REACT_APP_FIREBASE_APPID,
+};
+
+export default firebaseConfig;
